99d58a631f0903c4bb9269f8caaeb5be53050da6
Marforius <marforius2@gmail.com>
2012-06-26 20:43:00 -0700

    Transparent background added to MainWindow LauncherAlertBox, blends better

--- Emerald Nightmare Launcher/Emerald Nightmare Launcher/Main_Window.h
+++ Emerald Nightmare Launcher/Emerald Nightmare Launcher/Main_Window.h
@@ -731,7 +731,6 @@ namespace EmeraldNightmareLauncher {
 					static_cast<System::Byte>(0)));
 				this->LauncherAlertBox->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(64)), 
 					static_cast<System::Int32>(static_cast<System::Byte>(0)));
-				this->LauncherAlertBox->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"LauncherAlertBox.Image")));
 				this->LauncherAlertBox->LinkColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(64)), 
 					static_cast<System::Int32>(static_cast<System::Byte>(0)));
 				this->LauncherAlertBox->Location = System::Drawing::Point(-3, 449);
