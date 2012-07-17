f21d161cc96154fb40181e0f919481f518d76c1d
Marforius <marforius2@gmail.com>
2012-07-16 22:45:18 -0700

    forgot a file

---
+++ Emerald Nightmare Launcher/Emerald Nightmare Launcher AutoUpdate/Main.cpp
@@ -0,0 +1,50 @@
+
+#include <boost/program_options.hpp>
+#include "Updater.h"
+#include <iostream>
+using namespace std;
+//************************************
+// Method:    main
+// FullName:  main
+// Access:    public 
+// Returns:   int
+// Qualifier:
+// Parameter: int argc
+// Parameter: char * argv[]
+// Args: --RunMain --help
+//************************************
+int main(int ac, char* av[])
+	{
+	if (ac == 1)
+		{
+		cout << "This program must be called by the launcher." << endl;
+		return 0;
+		}
+
+	namespace bpo = boost::program_options;
+
+
+	bpo::options_description options("command line options");
+	options.add_options() ("help", "Use -h or --help to list all arguments")
+		("RunMain", "to be called from launcher, will delete files and update them via the web");
+	bpo::variables_map vmap;
+	try{
+		bpo::store(
+			bpo::parse_command_line(ac, av, options), vmap);
+		bpo::notify(vmap);
+
+
+
+		if (vmap.count("help")) {
+			cout << options << endl;
+			}
+
+		if (vmap.count("RunMain")) {
+			UpdateEverything();
+			}
+
+		}catch ( exception e ) // Not a valid arg
+		{
+		cout << options << endl;
+			}
+	}
